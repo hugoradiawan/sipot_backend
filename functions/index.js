6747c3a447f76ffa68145e58064e8b3d48990930
hugoradiawan <43243582+hugoradiawan@users.noreply.github.com>
2021-11-18 11:36:11 +0700

    debug

--- functions/index.js
+++ functions/index.js
@@ -3,109 +3,96 @@ const admin = require("firebase-admin");
 
 admin.initializeApp();
 
-async function createWithPhoneNumber(data, phoneNumber) {
-  if (!phoneNumber) {
-    throw new functions.https.HttpsError(
-      "phone-number-not-found",
-      "parameter phoneNumber wajib terisi"
-    );
-  }
+async function createWithPhoneNumber(data) {
   const userRecord = await admin.auth().createUser({
-    phoneNumber: phoneNumber,
+    phoneNumber: data.phoneNumber,
     displayName: data.displayName,
     disabled: false,
   });
   return userRecord;
 }
 
-async function createUserWithPhoneNumberAndEmail(data, phoneNumber) {
-  const newPhoneNumber = null;
-  if (phoneNumber != null || phoneNumber != "") {
-    newPhoneNumber = phoneNumber;
-  } else {
-    newPhoneNumber = data.phoneNumber;
+async function createUsersWithPhoneNumberAndEmail(dataList) {
+  const userRecordList = [];
+  for (let i = 0; i < dataList.length; i++) {
+    try {
+      const userRecord = await admin.auth().createUser({
+        email: dataList[i].email,
+        emailVerified: true,
+        phoneNumber: dataList[i].phoneNumber,
+        password: dataList[i].password,
+        displayName: dataList[i].displayName,
+        photoURL: dataList[i].photoURL,
+        disabled: false,
+      });
+      userRecordList.push(userRecord);
+    } catch (error) {
+      throw new functions.https.HttpsError(
+        "already-exists",
+        error,
+        dataList[i].email + " " + dataList[i].phoneNumber
+      );
+    }
   }
-  const userRecord = await admin.auth().createUser({
-    email: data.email,
-    emailVerified: true,
-    phoneNumber: newPhoneNumber,
-    password: "bpi12345",
-    displayName: data.displayName,
-    photoURL: data.photoURL,
-    disabled: false,
-  });
-  console.log("creating new user Succeded:", userRecord.uid);
-  admin
-    .firestore()
-    .collection("managerData")
-    .doc(userRecord.uid)
-    .set({
-      uid: userRecord.uid,
-      role: "manager",
-      phoneNumber: newPhoneNumber,
-      email: data.email,
-      displayName: data.displayName,
-      avatar: data.photoURL,
-      ProjekCodeList: data.ProjekCodeList,
-    })
-    .then((writeResult) => {
-      console.log("write Result:", writeResult);
-    });
-  return userRecord;
+  await updateManagerDataBatch(userRecordList);
+  return "succeed";
 }
 
-async function updateAccess(uid, ProjekId, phoneNumber) {
-  const writeResult = await admin
-    .firestore()
-    .collection("userData")
-    .doc(uid)
-    .set({
-      uid: uid,
-      projectId: ProjekId,
-      phoneNumber: phoneNumber,
-      docId: "",
-      displayName: "new TaskCompleter",
-      avatar: "",
-    });
-  return writeResult;
+async function updateAccessBatch(userList, ProjekId) {
+  const db = admin.firestore();
+  const colRef = db.collection("userData");
+  await db.runTransaction(async (t) => {
+    for (let i = 0; i < userList.length; i++) {
+      const docRef = colRef.doc(userList[i][0]);
+      t.set(docRef, {
+        uid: userList[i][0],
+        projectId: ProjekId,
+        phoneNumber: userList[i][1],
+        docId: "",
+        displayName: "",
+        avatar: "",
+      });
+    }
+  });
+}
+
+async function updateManagerDataBatch(userRecordList) {
+  const db = admin.firestore();
+  const colRef = db.collection("managerData");
+  await db.runTransaction(async (t) => {
+    for (let i = 0; i < userRecordList.length; i++) {
+      const docRef = colRef.doc(userRecordList[i].uid);
+      t.set(docRef, {
+        uid: userRecordList[i].uid,
+        role: "manager",
+        phoneNumber: userRecordList[i].phoneNumber,
+        email: userRecordList[i].email,
+        displayName: userRecordList[i].displayName,
+        avatar: userRecordList[i].photoURL,
+        ProjekCodeList: [],
+      });
+    }
+  });
 }
 
 exports.createUserWithPhoneNumberAndEmail = functions
   .region("asia-southeast2")
-  .https.onCall((data) => {
-    createUserWithPhoneNumberAndEmail(data);
-  });
-// param
+  .https.onCall(createUsersWithPhoneNumberAndEmail);
+
 exports.assignTaskCompleter = functions
   .region("asia-southeast2")
   .https.onCall(async (data) => {
-    const phoneNumberList = data.phoneNumberList;
-    for (let i = 0; i < phoneNumberList.length; i++) {
+    const userList = [];
+    for (let i = 0; i < data.nameAndPhoneNumberList.length; i++) {
+      var userRecord;
       try {
-        const userRecord = await admin
+        userRecord = await admin
           .auth()
-          .getUserByPhoneNumber(phoneNumberList[i]);
-        console.log("already exist");
-        const writeResult = await updateAccess(
-          userRecord.uid,
-          data.ProjekId,
-          phoneNumberList[i]
-        );
-        console.log("write Result:", writeResult);
-        console.log("phoneNumber:", phoneNumberList[i]);
+          .getUserByPhoneNumber(dataList.nameAndPhoneNumberList[i].phoneNumber);
       } catch (error) {
-        const userRecord = await createWithPhoneNumber(
-          data,
-          phoneNumberList[i]
-        );
-        const writeResult = await updateAccess(
-          userRecord.uid,
-          data.ProjekId,
-          phoneNumberList[i]
-        );
-        console.log("no PhoneNumber found");
-        console.log("write Result:", writeResult);
-        console.log("phoneNumber:", phoneNumberList[i]);
+        userRecord = await createWithPhoneNumber(dataList.nameAndPhoneNumberList[i]);
       }
+      userList.push([userRecord.uid, phoneNumberList[i]]);
     }
+    await updateAccessBatch(userList, data.ProjekId);
   });
